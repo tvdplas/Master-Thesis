bcf238eda43240d123b8154480b4fd19df7075ad
Thomas van der Plas <thomasvanderplas.leiden@gmail.com>
2025-09-15 10:27:14 +0200

    fix gurobi logs

--- src/E-VCSP/Formatting/CustomGRBCallback.cs
+++ src/E-VCSP/Formatting/CustomGRBCallback.cs
@@ -5,7 +5,7 @@ namespace E_VCSP.Formatting {
         protected override void Callback() {
             if (where == GRB.Callback.MESSAGE && Config.CONSOLE_GUROBI) {
                 string text = GetStringInfo(GRB.Callback.MSG_STRING);
-                text.Substring(0, text.Length - 1);
+                text = text.Substring(0, text.Length - 1); // remove \n which is baked into grb text
                 Console.WriteLine(text);
             }
         }
