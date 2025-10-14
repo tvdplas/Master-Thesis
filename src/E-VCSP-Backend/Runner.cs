cdf81a7b209d1737b0577a0ce3e6149c3d138fbb
Thomas van der Plas <thomasvanderplas.leiden@gmail.com>
2025-10-14 09:54:54 +0200

    update timeouts for server run

--- src/E-VCSP-Backend/Runner.cs
+++ src/E-VCSP-Backend/Runner.cs
@@ -253,9 +253,9 @@ namespace E_VCSP_Backend {
         void expVCSPRounds() {
             Reload("VCSP Rounds");
 
-            Config.VSP_SOLVER_TIMEOUT_SEC = 50;
-            Config.CSP_SOLVER_TIMEOUT_SEC = 50;
-            Config.VCSP_SOLVER_TIMEOUT_SEC = 100;
+            Config.VSP_SOLVER_TIMEOUT_SEC = 600;
+            Config.CSP_SOLVER_TIMEOUT_SEC = 600;
+            Config.VCSP_SOLVER_TIMEOUT_SEC = 900;
             Config.VSP_LB_SEC_COL_ATTEMPTS = 4;
             Config.VSP_LB_SEC_COL_COUNT = 4;
             Config.CSP_LB_SEC_COL_ATTEMPTS = 8;
